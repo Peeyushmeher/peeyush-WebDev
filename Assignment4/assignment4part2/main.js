c2072f60e317c7ecb00204d213c9e3857e77094a
Peeyushmeher <meherpeeyush@gmail.com>
2024-04-03 21:34:54 -0400

    Second commit

--- Assignment4/assignment4part2/main.js
+++ Assignment4/assignment4part2/main.js
@@ -2,7 +2,7 @@
 	// File: index.html
 	// Date: April 4 2024 
     // Purpose: This is a image gallary 
-    
+
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -10,8 +10,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const imageAltText = { 
+    'pic1.jpg': 'human eye', 
+    'pic2.jpg': 'waves', 
+    'pic3.jpg': 'flowers', 
+    'pic4.jpg': 'hieroglyplics ', 
+    'pic5.jpg': 'butterfly'  
+  };
 
 /* Looping through images */
 

03c18b3847b2ea69e0b77e1af6229e7a23499e39
Peeyushmeher <meherpeeyush@gmail.com>
2024-04-03 21:35:16 -0400

    javascript

---
+++ Classnotes/javascript/intro/script.js
@@ -0,0 +1,27 @@
+console.log("my script is running ");
+
+var myString = "var :Intro to JavaScript";
+
+// let myString2 = "Let = Intro to JavaScript";
+
+// const myString3 = "const = Intro to JavaScript";
+
+// myString4 = "= Intro to JavaScript"
+
+
+console.log(myString.toLocaleUpperCase());
+// console.log(myString2);
+// console.log(myString3);
+// console.log(myString4);
+
+
+
+let a = 5 
+let b = 10
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(math.sqrt(a+b))
+console.log(math.sqrt(a+b))

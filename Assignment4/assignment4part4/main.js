f0b695979b4698b23a283dad105ea553e8660f12
Peeyushmeher <meherpeeyush@gmail.com>
2024-04-04 01:40:09 -0400

    fourth commmit

--- Assignment4/assignment4part4/main.js
+++ Assignment4/assignment4part4/main.js
@@ -70,7 +70,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -82,6 +82,77 @@ class Ball extends Shape {
     }
   }
 }
+
+//EvilCircle
+class EvilCircle extends Shape {
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = 'white';
+    this.size = 10;
+
+    // Set up keyboard controls
+    window.addEventListener('keydown', (e) => {
+      switch (e.key) {
+        case 'a':
+          this.x -= this.velX;
+          break;
+        case 'd':
+          this.x += this.velX;
+          break;
+        case 'w':
+          this.y -= this.velY;
+          break;
+        case 's':
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3; // Make the stroke thicker
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if ((this.x + this.size) >= width) {
+      this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+      this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+      this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+      this.y += this.size;
+    }
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false; // The ball is 'eaten'
+          ballCount--; // Decrease the count of balls
+          ballCountPara.textContent = `Ball count: ${ballCount}`;
+        }
+      }
+    }
+  }
+}
+
+
 
 const balls = [];
 
